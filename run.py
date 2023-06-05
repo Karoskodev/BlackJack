725af722565b0ea6bc22133da5c63a19b0e95e60
Karosko dev <t.karavasilev@gmail.com>
2023-06-05 21:24:06 +0000

    Rename cards list to deck.Add dealing function to deal card and remove it from deck

--- run.py
+++ run.py
@@ -6,3 +6,11 @@ cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A", 2, 3, 4, 5, 6, 7, 8, 9,
 player = []
 
 dealer = []
+
+def dealing (who):
+    """
+    Deal a card to the player/dealer and remove that card from cards
+    """
+    card = random.choice(cards)
+    who.append(card)
+    cards.remove(card)
