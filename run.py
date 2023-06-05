aad61a9b619c327dc61eac4e95ae0190040cadcc
Karosko dev <t.karavasilev@gmail.com>
2023-06-05 21:07:13 +0000

    Import random module and add cards list(deck) and empty lists player,dealer to store the dealt cards

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A",
+         2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+
+player = []
+
+dealer = []
