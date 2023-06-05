beb096c7904e28bb491b5efc9f5ba3f66e3b68fc
Karosko dev <t.karavasilev@gmail.com>
2023-06-05 21:59:25 +0000

    Add calculate score function to calculate score from cards

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import random
 
-cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A",
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A",
          2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 
 player = []
@@ -9,8 +9,28 @@ dealer = []
 
 def dealing (who):
     """
-    Deal a card to the player/dealer and remove that card from cards
+    Deal a card to the player/dealer and remove that card from deck
     """
-    card = random.choice(cards)
+    card = random.choice(deck)
     who.append(card)
-    cards.remove(card)
+    deck.remove(card)
+
+
+def calculate_score(cards):
+    """
+    Takes a list of cards and returns the score calculated from the cards
+    """
+    score = 0
+    high = ["j", "Q", "K"]
+    for card in cards:
+        if card in range(1, 11):
+            score += card
+        elif card in high:
+            score += 10
+        else:
+            if score > 11:
+                score += 1
+            else:
+                score += 11
+    return score
+
