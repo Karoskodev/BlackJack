b4faa97eff7ff2e2cf6b69648ff2fc87c0fdd16c
Karosko dev <t.karavasilev@gmail.com>
2023-06-11 17:00:14 +0000

    Add coments

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import random
 
+# Define the deck of cards
 deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A",
         2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A",
         2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A",
         2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 
+# Player and dealer hands
 player = []
 
 dealer = []
@@ -64,6 +66,7 @@ def main():
     Function to run the game.
     """
 
+    # Deal two cards to the player and dealer
     for _ in range(2):
         dealing(player)
         dealing(dealer)
@@ -71,20 +74,25 @@ def main():
     game_over = False
 
     while not game_over:
+        # Calculate scores for the player and dealer
         player_score = calculate_score(player)
         dealer_score = calculate_score(dealer)
 
+        # Display the player's cards and score
         print(f"\nYour cards:")
         for x in player:
             print(f"{x},", end=" ")
         print(f"\nscore: {player_score}")
 
+        # Show the dealer's first card
         print(f"\nDealer's first card: \n{dealer[0]}\n")
 
+        # Check if any of the end-game conditions are met
         if player_score == 21 or dealer_score == 21 or player_score > 21:
             game_over = True
         else:
             try:
+                # Prompt the player to either get another card or no
                 ask = input("Type 'y' to get another card, or 'n' to pass: ")
 
                 if ask.lower() == "y":
@@ -92,15 +100,18 @@ def main():
                 elif ask.lower() == "n":
                     game_over = True
                 else:
+                    # Raise a ValueError if the input is not 'y' or 'n'
                     raise ValueError
                     (f"I am sorry but {ask} is not a valid selection")
             except ValueError:
                 print("Invalid input. Please enter 'y' or 'n'")
 
+    # Dealer keeps getting cards until the score reaches 17 or above
     while dealer_score < 17:
         dealing(dealer)
         dealer_score = calculate_score(dealer)
 
+    # Display the final hands and scores of the player and dealer
     print(f"\nYour final hand:")
     for x in player:
         print(f"{x},", end=" ")
@@ -110,17 +121,21 @@ def main():
     for x in dealer:
         print(f"{x},", end=" ")
     print(f"\nfinal score: {dealer_score}\n")
+
+    # Compare the scores and determine the winner
     print(compare_score(player_score, dealer_score))
 
 
 print("Welcome to Blackjack!")
 
 
+# Main game loop that allows the player to start a new game or exit
 while True:
     try:
         question = input("\nDo you want to start a new game?"
-                         "Type 'y' for yes or 'n' for no:")
+                         " Type 'y' for yes or 'n' for no:")
         if question.lower() == 'y':
+            # If less than 26 cards remains in deck, reshuffle the deck
             if len(deck) < 26:
                 deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A",
                         2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A",
